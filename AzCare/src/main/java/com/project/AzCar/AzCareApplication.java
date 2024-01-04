929c3430234eaca0ad43b95734ec2812b43d7ed0
levankydu <32994663+levankydu@users.noreply.github.com>
2024-01-04 12:52:17 +0700

    04/01/2024
    first commit

---
+++ AzCare/src/main/java/com/project/AzCar/AzCareApplication.java
@@ -0,0 +1,13 @@
+package com.project.AzCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzCareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzCareApplication.class, args);
+	}
+
+}
