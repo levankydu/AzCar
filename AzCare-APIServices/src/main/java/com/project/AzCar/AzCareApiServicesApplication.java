b3c6c430c8fd5c0b60967904b079a0194a116cd9
levankydu <32994663+levankydu@users.noreply.github.com>
2024-01-31 21:00:22 +0700

    gắn 1 vài template , 31/01/24
    ok

---
+++ AzCare-APIServices/src/main/java/com/project/AzCar/AzCareApiServicesApplication.java
@@ -0,0 +1,13 @@
+package com.project.AzCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzCareApiServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzCareApiServicesApplication.class, args);
+	}
+
+}
