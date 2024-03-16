ba712a7930d5a6f1dd03574500a6262d073cea6b
levankydu <32994663+levankydu@users.noreply.github.com>
2024-03-17 00:17:32 +0700

    16/3

--- AzCare-APIServices/src/main/java/com/project/AzCar/Entities/Cars/CarInfor.java
+++ AzCare-APIServices/src/main/java/com/project/AzCar/Entities/Cars/CarInfor.java
@@ -5,13 +5,9 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.project.AzCar.Entities.Locations.Addreess;
-
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
